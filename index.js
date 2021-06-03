7950fd15482e3e1bdfb83ac5c106d81312c4058c
NoufAlnuaimi <77782468+NoufAlnuaimi@users.noreply.github.com>
2021-06-04 00:10:54 +0400

    uploading files

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function changeColors() {
+  var colorChange3 = document.getElementById("subtopic")
+  colorChange3.style.color = "orange";
+}
+function changeColors2() {
+  var colorChange4 = document.getElementById("subtitle")
+  colorChange4.style.color = "#20B2AA";
+}
+function Changes(){
+changeColors();
+changeColors2();
+}
+function showcontacts(){
+  var showcontacts = document.getElementById("links")
+  showContacts.style.backgroundcolor = "pink";
+}
